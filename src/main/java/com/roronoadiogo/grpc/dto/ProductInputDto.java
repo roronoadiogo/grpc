dd436bf0869dbe576bc0b692b3708f3710f2d302
Vinícius Diogo Alves <roronoadiogo@gmail.com>
2023-03-23 23:12:00 -0300

    feat: adding validation to records dtos

--- src/main/java/com/roronoadiogo/grpc/dto/ProductInputDto.java
+++ src/main/java/com/roronoadiogo/grpc/dto/ProductInputDto.java
@@ -1,8 +1,21 @@
 package com.roronoadiogo.grpc.dto;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 import java.util.Objects;
 
-public record ProductInputDto(String name, Double price, Integer quantityInStock) {
+public record ProductInputDto(
+        @NotBlank(message = "Product name cannot be blank")
+        @Size(min = 8, message = "Specify a name more precise for your product") String name,
+        @DecimalMin(value = "0.1", message = "You need to have a minimum value")
+        @NotBlank(message = "Need specify a value") Double price,
+        @Min(value = 1, message = "Need specify a min stock")
+        @NotBlank(message = "Stock cannot be empty")
+        Integer quantityInStock) {
+
     public ProductInputDto {
         Objects.requireNonNull(name);
         Objects.requireNonNull(price);
